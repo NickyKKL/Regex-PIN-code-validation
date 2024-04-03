32b779ae222243097bef5307de0abde34765a76d
Nikita Koval <n.koval@sh.dunice.net>
2024-04-03 11:54:02 +0300

    Completed Task

---
+++ PinCodeValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Regex_PIN_code_validation;
+
+public class PinCodeValidator
+{
+    public static bool Validator(string code)
+    {
+        string pattern = @"^(?=.*q)(\S{6}|\S{10})$";
+
+        if (Regex.IsMatch(code, pattern))
+        {
+            Console.WriteLine($"{code} --> {true}");
+            return true;
+        }
+
+        Console.WriteLine($"{code} --> {false}");
+        return false;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Regex_PIN_code_validation;
+
+PinCodeValidator.Validator("12345q");
+PinCodeValidator.Validator("12345");
+PinCodeValidator.Validator("045654q345");
